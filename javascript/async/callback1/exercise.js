70633d57a08f0900496b11ef12597f7a4a6bfe26
Victor M <victormilite96@gmail.com>
2024-04-23 10:08:18 +0200

    terminado ejercicio 5

---
+++ javascript/async/callback1/exercise.js
@@ -0,0 +1,8 @@
+function printAsyncName (callback, name) {
+let id1= setInterval(callback, 1000);
+let id2= setInterval(() => console.log(name), 2000);
+setTimeout(() => clearInterval(id1), 1000);
+setTimeout(() => clearInterval(id2), 2000);
+}
+
+printAsyncName(() => console.log("hello"), "peter")
