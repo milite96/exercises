1603548002cc3762b08733cae3b31010c985f631
Victor M <victormilite96@gmail.com>
2024-06-12 23:12:19 +0200

    corregido errores ejercicio32

--- javascript/ejercicio32/json4.js
+++ javascript/ejercicio32/json4.js
@@ -12,7 +12,9 @@ const person = {
 //   }
 // );
 
-const filter = { id: person.id, age: person.age}
+const filter = { id: person.id, age: person.age }
+
+// we do this step in order to create a real copy from the person object
 const json = JSON.parse(JSON.stringify(filter));
 
 console.log(json); // Should return: { id: 1, age: 25 }
