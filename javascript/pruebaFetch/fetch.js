5de5a7b364ebb29168729522416cd51d3488f192
Victor M <victormilite96@gmail.com>
2024-06-12 16:35:05 +0200

    fetch de freetogame

--- javascript/pruebaFetch/fetch.js
+++ javascript/pruebaFetch/fetch.js
@@ -1,5 +1,9 @@
 async function fetchApi(){
-fetch("https://cors-anywhere.herokuapp.com/www.freetogame.com/api/games")
+    const headers = {'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
+	'x-rapidapi-key': '2e2086454amsh66a83740e5d1077p14df82jsn13b968e9f334'}
+fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
+    mode: 'cors', headers
+})
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(error => console.error("Error: ", error))
