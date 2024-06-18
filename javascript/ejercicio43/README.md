e551b91d08286b05eca6926ba79011400f11cd92
Victor Milite <victormilite96@gmail.com>
2024-06-18 18:47:24 +0200

    ejercicio43 terminado

---
+++ javascript/ejercicio43/README.md
@@ -0,0 +1,3 @@
+# Local Storage
+
+Starting from the previous exercise, write a method that recovers the data saved in localStorage and prints them in the console.
---
+++ javascript/ejercicio43/localStorage2.js
@@ -0,0 +1,23 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+function setUserToLocalStorage(user) {
+  const stringifyUser = JSON.stringify(user);
+  localStorage.setItem("user", stringifyUser);
+}
+
+function getUserFromLocalStorage(user){
+  setTimeout(() => {
+    const getUserJson = localStorage.getItem(user)
+    const parseUserInfo = JSON.parse(getUserJson)
+    console.log(parseUserInfo);
+    return parseUserInfo;
+  }, 500);
+}
+
+
+setUserToLocalStorage(user);
+getUserFromLocalStorage("user");
