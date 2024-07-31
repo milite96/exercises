fede81c063aa5d3f113713ed4c15a3c9eba44791
Victor Milite <victormilite96@gmail.com>
2024-07-31 15:21:07 +0200

    added mars location

---
+++ httpServer/mars.mjs
@@ -0,0 +1,21 @@
+import { createServer } from "node:http"
+
+const data = {
+    location: "Mars"
+}
+
+const server = createServer((req, res) => {
+    console.log("request received");
+
+    console.log(req);
+
+    res.statusCode = 200;
+
+    res.setHeader("Content-Type", "application/json");
+    
+    res.end(JSON.stringify(data));
+})
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
